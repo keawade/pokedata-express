6c2ddfa9d7dc75d1c910423be99c752f9a792a3a
Keith Wade <keawade@gmail.com>
2017-01-05 21:04:27 -0600

    Implement type data and styling for types in viewer

--- routes/index.js
+++ routes/index.js
@@ -7,13 +7,33 @@ var {pokemon} = require('../data/pokemon.json');
 router.get('/', function (req, res, next) {
   const random = Math.floor(Math.random() * (152));
   const selected = pokemon.find((poke) => (Number(poke.id) === random));
+  const strengths = calculateStrengths(selected.types)
+  selected.weaknesses = Object.keys(strengths).filter((key) => (strengths[key] >= 2));
+  selected.resistances = Object.keys(strengths).filter((key) => (strengths[key] == 0.5 || strengths[key] == 0.25));
+  selected.immunities = Object.keys(strengths).filter((key) => (strengths[key] == 0));
   console.log(selected)
   res.render('pokemon', { selected, pokemon });
 });
 
 router.get('/:id', function (req, res, next) {
   const selected = pokemon.find((poke) => (Number(poke.id) === Number(req.params.id)));
+  const strengths = calculateStrengths(selected.types)
+  selected.weaknesses = Object.keys(strengths).filter((key) => (strengths[key] >= 2));
+  selected.resistances = Object.keys(strengths).filter((key) => (strengths[key] == 0.5 || strengths[key] == 0.25));
+  selected.immunities = Object.keys(strengths).filter((key) => (strengths[key] == 0));
+  console.log(selected)
   res.render('pokemon', { selected, pokemon });
 })
 
 module.exports = router;
+
+const { types } = require('../data/types.json');
+
+const typesKeys = Object.keys(types)
+
+const calculateStrengths = (givenTypes) => {
+  return typesKeys.reduce((strengths, type) => {
+    strengths[type] = givenTypes.reduce((total, givenType) => total * types[type][givenType.toLowerCase()], 1)
+    return strengths
+  }, {})
+}
